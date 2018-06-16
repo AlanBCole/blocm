3a67b48f2c79d234a22285ff2fd92b73f6f849d1
Alan <alantuneit@gmail.com>
2018-06-16 20:54:57 +0000

    commit

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import { fuckOff } from './another';
-
-var fuckWebPack = "Webpack, I know you are pretty cool but your communication skills really suck! I mean it!!";
-
-const screwWebPack = "Listening to you can make me want to punch you in the face. I have wasted way too much time trying to figure out your explanations :(";
-
-fuckOff(fuckWebPack, screwWebPack);
